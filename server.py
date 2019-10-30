c6f6691b1712b8063b444c84388143e091241f4b
Aaron Pradhan <12a.pradhan@gmail.com>
2019-10-30 12:46:21 -0400

    Updated requirement versions

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 
 from start import get_media_list
 
-app = Flask(__name__, static_url_path = "")
+app = Flask(__name__, static_url_path="")
 
 
 @app.route("/api/load", methods=['POST'])
